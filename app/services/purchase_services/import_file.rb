072f034414dd587e42a6a7f1a3f422707d7f2f82
Thiago Ribeiro <thiagitosouza@gmail.com>
2019-02-19 23:22:55 -0300

    add rubocop and travis.ci

--- app/services/purchase_services/import_file.rb
+++ app/services/purchase_services/import_file.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
-class PurchaseServices::ImportFile
-  require 'csv'
-  def import_csv(file)
-    return false if file.blank?
+module PurchaseServices
+  class ImportFile
+    require 'csv'
+    def import_csv(file)
+      return false if file.blank?
 
-    purchases = []
-    ::CSV.foreach(file, col_sep: "\t", headers: true).each do |row|
-      purchase = Purchase.create!(row.to_h.transform_keys { |key| key.to_s.tr(' ', '_') })
-      purchases << purchase
+      purchases = []
+      ::CSV.foreach(file, col_sep: "\t", headers: true).each do |row|
+        purchase = Purchase.create!(row.to_h.transform_keys { |key| key.to_s.tr(' ', '_') })
+        purchases << purchase
+      end
+      total_revenue(purchases)
     end
-    total_revenue(purchases)
-  end
 
-  def total_revenue(purchases)
-    purchases.sum { |purchase| purchase.item_price * purchase.purchase_count }
+    def total_revenue(purchases)
+      purchases.sum { |purchase| purchase.item_price * purchase.purchase_count }
+    end
   end
 end
